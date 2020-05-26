4a6ea8abe322724b0a46708505315b819543bbd2
Julia Shuieh <crystallineblade@gmail.com>
2020-05-26 15:02:53 -0700

    Added installed MSI script

---
+++ MsiChecker.ps1
@@ -0,0 +1,13 @@
+﻿$result = Get-WmiObject -Class Win32_Product
+
+Write-Output "Installed MSIs:"
+$result | Format-List Name, Version
+
+$appName = "InstEd"
+$exists = $result | Where {$_.Name -like 'InstEd*' }
+
+if ($exists -ne $null) {
+    Write-Output "$appName is installed on this device."
+} else {
+    Write-Output "$appName is not installed on this device."
+}
